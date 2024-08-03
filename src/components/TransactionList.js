6f61dc11b40130861ef1ebeecd2898158f920d1a
franklynekirwa2 <sangkirwa2@gmail.com>
2024-08-03 18:39:57 +0300

    adddata

---
+++ src/components/TransactionList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function TransactionList({date,description,category,amount}) {
+  return (
+
+    <tr>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+    </tr>
+
+  );
+}
+
+export default TransactionList;
